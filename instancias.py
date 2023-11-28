d08cee885d387eeac7b9cc83cc453cb8f2c37753
Daniel Ojeda <ojedadaniel178@gmial.com>
2023-11-27 19:13:52 -0500

    calculos para rectangulos

---
+++ instancias.py
@@ -0,0 +1,12 @@
+from Cuadrado import Cuadrado 
+from Rectangulo import rectangulo
+
+
+
+
+r1=rectangulo(12.32)
+print ("el area del rectangulo es: r1.area()")
+print ("el perimetro del rectangulo es: r1.perimetro()")
+
+
+
